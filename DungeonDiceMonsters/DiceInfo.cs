4769d360bd771e93c74c11ec9c625b7625c84023
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2023-09-01 15:33:26 -0400

    CardInfo/DiceInfo and cardlistdb json drafts
    CardInfo and DiceInfo objects created; first draft for the DB json file.

---
+++ DungeonDiceMonsters/DiceInfo.cs
@@ -0,0 +1,43 @@
+﻿//Joel Campos
+//9/1/2023
+//DiceInfo Class
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Activation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonDiceMonsters
+{
+    public class DiceInfo
+    {
+        public DiceInfo(int level, string[] crests, int[] values)
+        {
+            _Level = level;
+            for(int x = 0; x < 6; x++)
+            {
+                _Crests[x] = crests[x];
+                _Values[x] = values[x];
+            }
+        }
+
+        public int Level
+        {
+            get { return _Level; }
+        }
+        public string Crest(int index)
+        {
+            return _Crests[index];
+        }
+        public int Value(int index)
+        {
+            return _Values[index];
+        }
+
+        private int _Level;
+        private string[] _Crests = new string[6];
+        private int[] _Values = new int[6];
+    }
+}
