272a684f8a23d1d73db69cfa19f9a2a687764219
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-05-08 23:07:27 -0400

    Stablishing my PC as the game Server for now

--- DungeonDiceMonsters/Forms/PvPMenu.cs
+++ DungeonDiceMonsters/Forms/PvPMenu.cs
@@ -50,7 +50,7 @@ namespace DungeonDiceMonsters
             //Connect
             btnExit.Visible = false;
             btnFindMatch.Visible = false;
-            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPAddress ip = IPAddress.Parse("192.168.0.220");
             int port = 5000;
             TcpClient client = new TcpClient();
 
