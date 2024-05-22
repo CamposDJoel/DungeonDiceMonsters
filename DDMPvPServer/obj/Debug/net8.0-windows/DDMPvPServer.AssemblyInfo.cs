6ef123649d802372d8ad66dc202f95a4b948cab3
CamposDJoel <140183065+CamposDJoel@users.noreply.github.com>
2024-05-22 17:52:56 -0400

    Rebuilding the solution

--- DDMPvPServer/obj/Debug/net8.0-windows/DDMPvPServer.AssemblyInfo.cs
+++ DDMPvPServer/obj/Debug/net8.0-windows/DDMPvPServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDMPvPServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24803eefde65e05054726328a3ec5d4de35144f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f955a1a93fe04000d233ce190365b666936d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDMPvPServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDMPvPServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
